b418a9f24ec46547828b2ab320f08e468c482072
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-12-03 20:08:39 +0300

    play pause

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-var ids = $('.info').data('ids');
-
-console.log(ids);
+    currentPlaylist = $('.info').data('ids');
+    audioElement = new Audio();
+    setTrack(currentPlaylist[0], currentPlaylist, false);
 
 });
+
+function setTrack(trackId, newPlaylist, play) {
+    audioElement.setTrack("/music/bensound-goinghigher.mp3");
+
+    if (play) {
+        audioElement.play();
+    }
+
+}
+
+function playSong() {
+    $('.controlButton.play').hide();
+    $('.controlButton.pause').show();
+    audioElement.play();
+}
+
+function pauseSong() {
+    $('.controlButton.pause').hide();
+    $('.controlButton.play').show();
+    audioElement.pause();
+}
